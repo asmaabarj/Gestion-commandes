7f93c16d2230e0156b8b039e2c9d5a9e6b0ac855
anouar <anouar.ab95@gmail.com>
2024-10-14 16:41:13 +0100

    GES-16 <realise l'architecture  >

--- src/main/java/gestion_commande/models/Admin.java
+++ src/main/java/gestion_commande/models/Admin.java
@@ -1,5 +1,34 @@
 package gestion_commande.models;
 
-public class Admin {
+import gestion_commande.enums.Role;
 
+public class Admin extends Utilisateur {
+    
+    private Integer niveauAcces;
+    
+    public Admin() {
+        super();
+         setRole(Role.Admin);
+
+    }
+    
+    public Integer getNiveauAcces() {
+        return niveauAcces;
+    }
+    
+    public void setNiveauAcces(Integer niveauAcces) {
+        this.niveauAcces = niveauAcces;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "id=" + getId() +
+                ", nom='" + getNom() + '\'' +
+                ", prenom='" + getPrenom() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", role=" + getRole() +
+                ", niveauAcces=" + niveauAcces +
+                '}';
+    }
 }
--- src/main/java/gestion_commande/models/Utilisateur.java
+++ src/main/java/gestion_commande/models/Utilisateur.java
@@ -1,5 +1,72 @@
 package gestion_commande.models;
 
-public class Utilisateur {
+import gestion_commande.enums.Role;
+
+public abstract class Utilisateur {
+
+    private Long id;
+
+    private String nom;
+
+    private String prenom;
+
+    private String email;
+
+    private String motDePasse;
+
+    private Role role;
+    
+    public Utilisateur() {
+		// TODO Auto-generated constructor stub
+	}
+    
+    public Long getId() {
+		return id;
+	}
+    
+    public String getEmail() {
+		return email;
+	}
+    public String getMotDePasse() {
+		return motDePasse;
+	}
+    public String getNom() {
+		return nom;
+	}
+    public String getPrenom() {
+		return prenom;
+	}
+    public Role getRole() {
+		return role;
+	}
+    public void setEmail(String email) {
+		this.email = email;
+	}
+    public void setId(Long id) {
+		this.id = id;
+	}
+    public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+    public void setNom(String nom) {
+		this.nom = nom;
+	}
+    public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+    public void setRole(Role role) {
+		this.role = role;
+	}
+    
+    @Override
+    public String toString() {
+        return "Utilisateur{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                ", role=" + role +
+                '}';
+    }
 
 }
