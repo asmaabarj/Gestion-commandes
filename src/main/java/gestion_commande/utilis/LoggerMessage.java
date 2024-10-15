0822094ca47d7f337a8ea3ba8c2d2dec3bdddfe8
anouar <anouar.ab95@gmail.com>
2024-10-15 14:27:48 +0100

    GES-22 <genner Logger>

--- src/main/java/gestion_commande/utilis/LoggerMessage.java
+++ src/main/java/gestion_commande/utilis/LoggerMessage.java
@@ -1,5 +1,32 @@
 package gestion_commande.utilis;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class LoggerMessage {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerMessage.class);
+
+    public  LoggerMessage(){
+
+    }
+
+    public static void info(String message) {
+        logger.info(message);
+    }
+
+    public static void warn(String message) {
+        logger.warn(message);
+    }
+    public static  void debug(String message){
+        logger.debug(message);
+        
+    }
+
+
+    public static void error(String message) {
+        logger.error(message);
+    }
+
 
 }
