cc5babade6b9ce0c0a6cb3e1667c22a7d914de11
anouar <anouar.ab95@gmail.com>
2024-10-17 12:01:28 +0100

    fin task poduit

--- src/main/java/gestion_commande/controllers/ProduitServlet.java
+++ src/main/java/gestion_commande/controllers/ProduitServlet.java
@@ -37,7 +37,7 @@ public class ProduitServlet extends HttpServlet {
 		templateResolver.setTemplateMode("HTML");
 
 		templateEngine = new TemplateEngine();
-		templateEngine.addDialect(new LayoutDialect()); // Ajoutez cette ligne
+		templateEngine.addDialect(new LayoutDialect()); 
 		templateEngine.addTemplateResolver(templateResolver);
 	}
 
@@ -76,7 +76,7 @@ public class ProduitServlet extends HttpServlet {
 		context.setVariable("produits", produits);
 		context.setVariable("currentPage", page);
 		context.setVariable("totalPages", totalPages);
-
+		context.setVariable("title", "Produit"); 
 		response.setContentType("text/html;charset=UTF-8");
 		templateEngine.process("produit", context, response.getWriter());
 	}
