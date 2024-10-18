15be135175f3a31b5e7e5a02b5e8229ab1327562
anouar <anouar.ab95@gmail.com>
2024-10-18 11:19:49 +0100

    genner doessier TemplateEngineUtil

--- src/main/java/gestion_commande/controllers/ProduitServlet.java
+++ src/main/java/gestion_commande/controllers/ProduitServlet.java
@@ -69,7 +69,6 @@ public class ProduitServlet extends HttpServlet {
 		context.setVariable("produits", produits);
 		context.setVariable("currentPage", page);
 		context.setVariable("totalPages", totalPages);
-
 		response.setContentType("text/html;charset=UTF-8");
 		templateEngine.process("produit", context, response.getWriter());
 	}
