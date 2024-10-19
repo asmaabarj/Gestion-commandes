c1befa4744b26ce500bc9cf019efc6349164a53e
asmaabarj <asmaabarj5@gmail.com>
2024-10-19 23:36:15 +0100

    le button de logout

--- src/main/java/gestion_commande/controllers/LoginServlet.java
+++ src/main/java/gestion_commande/controllers/LoginServlet.java
@@ -2,29 +2,26 @@ package gestion_commande.controllers;
 
 import java.io.IOException;
 import java.util.Optional;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
-import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
-
 import gestion_commande.models.Admin;
 import gestion_commande.models.Client;
 import gestion_commande.services.AdminService;
 import gestion_commande.services.ClientService;
 import gestion_commande.utilis.PasswordUtil;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     private TemplateEngine templateEngine;
-    private AdminService adminServices; 
-    private ClientService clientServices;  
+    private AdminService adminServices;
+    private ClientService clientServices;
 
     public LoginServlet() {
         super();
@@ -41,13 +38,24 @@ public class LoginServlet extends HttpServlet {
         templateEngine = new TemplateEngine();
         templateEngine.addDialect(new LayoutDialect());
         templateEngine.addTemplateResolver(templateResolver);
-        
+
         adminServices = new AdminService();
-        clientServices = new ClientService(); 
+        clientServices = new ClientService();
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            if (session.getAttribute("admin") != null) {
+                response.sendRedirect(request.getContextPath() + "/admins");
+                return;
+            } else if (session.getAttribute("client") != null) {
+                response.sendRedirect(request.getContextPath() + "/products");
+                return;
+            }
+        }
+
         response.setContentType("text/html;charset=UTF-8");
         templateEngine.process("login", new org.thymeleaf.context.WebContext(request, response, getServletContext()), response.getWriter());
     }
@@ -62,8 +70,8 @@ public class LoginServlet extends HttpServlet {
             Admin admin = optionalAdmin.get();
             if (PasswordUtil.hashPassword(password).equals(admin.getMotDePasse())) {
                 HttpSession session = request.getSession();
-                session.setAttribute("admin", admin); 
-                response.sendRedirect(request.getContextPath() + "/admins");  
+                session.setAttribute("admin", admin);
+                response.sendRedirect(request.getContextPath() + "/admins");
                 return;
             }
         }
@@ -73,8 +81,8 @@ public class LoginServlet extends HttpServlet {
             Client client = optionalClient.get();
             if (PasswordUtil.hashPassword(password).equals(client.getMotDePasse())) {
                 HttpSession session = request.getSession();
-                session.setAttribute("client", client); 
-                response.sendRedirect(request.getContextPath() + "/products");  
+                session.setAttribute("client", client);
+                response.sendRedirect(request.getContextPath() + "/products");
                 return;
             }
         }
